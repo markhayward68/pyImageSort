304ebf2247f0b6b02199720fd74dc73d29664d27
Mark <101432681+markhayward68@users.noreply.github.com>
2022-03-17 13:50:15 +0000

    Add files via upload

--- pyMTImageSort.py
+++ pyMTImageSort.py
@@ -1,31 +1,30 @@
-#
 # Name: pyMTImageSort.py
 #
 # Date 17/03/2022
 #
 # Description: Takes image(s) and extracts text for later processing... 
 # add multithreading... or multiprocessing, whichever works best really 
+# intention is to run it all in a docker container 
+#  
+# https://hub.docker.com/r/clearlinux/tesseract-ocr 
 #
-# Requirements: sudo apt install tesseract-ocr
+# Requirements: pip install tesseract 
 # cmd line: tesseract image_path text_result.txt -l eng --psm 6
-#
 
 import os
 import multiprocessing 
 import pytesseract
 
-# CPUCOUNT = multiprocessing.cpu_count() / 2 
+# CPUCOUNT = int(multiprocessing.cpu_count()) / 2
 CPUCOUNT = 2 
 processCount = 0 
 jobs = [] 
-
 
 def OCRImage(i, fn): 
     output = pytesseract.image_to_string(fn)
 
     with open(fn + ".txt", 'w') as f:
         f.writelines(output)
-
 
 if __name__ == '__main__': 
     directory = os.getcwd()
@@ -34,12 +33,9 @@ if __name__ == '__main__':
         if os.path.isfile(filename):
             if filename.endswith(".jpg"):
                 if os.path.isfile(filename + ".txt"):  
-                    # do nothing... 
                     pass
                 else: 
                     if processCount in range(CPUCOUNT):
-                        p = multiprocessing.Process(target=OCRImage, args = (processCount, filename)) 
-                        
+                        p = multiprocessing.Process(target = OCRImage, args = (processCount, filename))                         
                         jobs.append(p)
-
                         p.start() 
